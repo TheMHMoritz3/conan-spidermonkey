97c579e6763f54849ca23fd6f951ef9a8573864a
TheMHMoritz3 <TheMHMoritz3@users.noreply.github.com>
2020-07-20 20:48:37 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -1,25 +1,28 @@
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
 
 class LibnameConan(ConanFile):
-    name = "libname"
+    name = "spidermonkey"
     description = "Keep it short"
     topics = ("conan", "libname", "logging")
     url = "https://github.com/bincrafters/conan-libname"
     homepage = "https://github.com/original_author/original_lib"
     license = "MIT"  # Indicates license type of the packaged library; please use SPDX Identifiers https://spdx.org/licenses/
     exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
+    generators = "make"
 
     settings = "os", "arch", "compiler", "build_type"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-
+    
+    download_url = "http://ftp.mozilla.org/pub/spidermonkey/prereleases/59/pre2/mozjs-59.0.0pre2.tar.bz2"
+        
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
-    _cmake = None
-
+    
+    version = "59.0.0pre2"
+    
     requires = (
         "zlib/1.2.11"
     )
@@ -29,27 +32,22 @@ class LibnameConan(ConanFile):
             del self.options.fPIC
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version])
-        extracted_dir = self.name + "-" + self.version
-        os.rename(extracted_dir, self._source_subfolder)
-
-    def _configure_cmake(self):
-        if not self._cmake:
-            self._cmake = CMake(self)
-            self._cmake.definitions["BUILD_TESTS"] = False  # example
-            self._cmake.configure(build_folder=self._build_subfolder)
-        return self._cmake
+        tools.get("http://ftp.mozilla.org/pub/spidermonkey/prereleases/59/pre2/mozjs-59.0.0pre2.tar.bz2")
 
     def build(self):
-        cmake = self._configure_cmake()
-        cmake.build()
+        os.mkdir(self._build_subfolder)
+        os.chdir(self._build_subfolder)
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.configure(configure_dir="../mozjs-59.0.0pre2/js/src/")
+        os.system('touch ../mozjs-59.0.0pre2/js/src/configure')
+        os.system('touch ./config.status')
+        autotools.make()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        cmake = self._configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
+        os.mkdir(self._build_subfolder)
+        os.chdir(self._build_subfolder)
+        autotools = AutoToolsBuildEnvironment(self)
+        autotools.install()
         include_folder = os.path.join(self._source_subfolder, "include")
         self.copy(pattern="*", dst="include", src=include_folder)
         self.copy(pattern="*.dll", dst="bin", keep_path=False)
